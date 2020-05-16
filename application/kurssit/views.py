3107a770229e38514e98ea7d3c207cd70489c638
Aare <aare212@hotmail.com>
2020-05-16 21:33:59 +0300

    etusivu, opetustarjonta, kurssin lisääminen

--- application/kurssit/views.py
+++ application/kurssit/views.py
@@ -6,24 +6,27 @@ from application.kurssit.models import Kurssi
 def kurssit_index():
     return render_template("kurssit/list.html", kurssit = Kurssi.query.all())
 
-@app.route("/kurssit/new/")
+@app.route("/kurssit/lisaauusikurssi/")
 def kurssit_form():
-    return render_template("kurssit/new.html")
+    return render_template("kurssit/lisaauusikurssi.html")
 
 @app.route("/kurssit/<kurssi_id>/", methods=["POST"])
 def kurssit_set_done(kurssi_id):
-
     k = Kurssi.query.get(kurssi_id)
     k.done = True
     db.session().commit()
-  
+    return redirect(url_for("kurssit_index"))
+
+@app.route("/kurssit/<kurssi_id>/", methods=["POST"])
+def kurssit_ilmoittaudu(kurssi_id):
+    k = Kurssi.query.get(kurssi_id)
+    k.ilmoittautuminen = True
+    db.session().commit()
     return redirect(url_for("kurssit_index"))
 
 @app.route("/kurssit/", methods=["POST"])
 def kurssit_create():
     k = Kurssi(request.form.get("name"))
-
     db.session().add(k)
     db.session().commit()
-  
     return redirect(url_for("kurssit_index"))

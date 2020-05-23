dc6d0d5b229a49ee8dc9dc77e14e5ac22413df6c
Aare <aare212@hotmail.com>
2020-05-23 21:53:23 +0300

    Base - Kurssi ja User yhdistetty

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,15 +1,12 @@
 from application import db
+from application.models import Base
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-class User(UserMixin, db.Model):
+class User(UserMixin, Base):
 
     __tablename__ = "account"
-
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
--- application/kurssit/models.py
+++ application/kurssit/models.py
@@ -1,14 +1,10 @@
 from application import db
+from application.models import Base
 
-class Kurssi(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Kurssi(Base):
 
     name = db.Column(db.String(144), nullable=False)
     enrolled = db.Column(db.Boolean, nullable=False) # kurssipaikan varaus
-#    done = db.Column(db.Boolean, nullable=False) # suoritettu
 
 #    account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
 #                           nullable=False)
@@ -16,4 +12,3 @@ class Kurssi(db.Model):
     def __init__(self, name):
         self.name = name
         self.enrolled = False # kurssipaikka ei varattu
-#        self.done = False # suoritettu
